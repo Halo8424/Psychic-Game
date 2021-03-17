4ebcd4517a7f94c7ffc59c0f6eda0589688bf936
Richard.C <r.conde@live.com>
2021-03-17 15:52:35 -0400

    wins, losses, guesses counters working and some spaceing styles added

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,31 +1,31 @@
 lettersArray = [
-    "A",
-    "B",
-    "C",
-    "D",
-    "E",
-    "F",
-    "G",
-    "H",
-    "I",
-    "J",
-    "K",
-    "L",
-    "M",
-    "N",
-    "O",
-    "P",
-    "Q",
-    "R",
-    "S",
-    "T",
-    "U",
-    "V",
-    "W",
-    "X",
-    "Y",
-    "Z"
- ];
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 // Global Variables
 let gameSelectedLetter = "";
@@ -35,43 +35,55 @@ let remainingUserGuesses = 10;
 let wrongUserGuessesArray = [];
 
 // Functions
-function startGame(){
-    generateRandomLetter();
-};
-
-function generateRandomLetter(){
-  let letter  = lettersArray[Math.floor(Math.random() * lettersArray.length)];
-    gameSelectedLetter = letter;
-    console.log(`Computer selected Letter: ${gameSelectedLetter}`);
-};
-
-function checkUserGuess(userInput){
-    if(userInput === gameSelectedLetter){
-        winCounter++;
-        console.log(`Wins: ${winCounter}`);
-        $("#winCounter").text(winCounter);
-        startGame();
-    }else{
-        wrongGuess(userInput);
-    }
-};
-
-
-function wrongGuess(userInput){
-    wrongUserGuessesArray.push(userInput);
-    loseCounter++;
-    console.log(`losses: ${loseCounter}`);
-    $("#lossCounter").text(loseCounter);
-    startGame();
+function startGame() {
+  generateRandomLetter();
 }
 
+function generateRandomLetter() {
+  let letter = lettersArray[Math.floor(Math.random() * lettersArray.length)];
+  gameSelectedLetter = letter;
+  console.log(`Computer selected Letter: ${gameSelectedLetter}`);
+  $("#guessesLeftCounter").text(remainingUserGuesses);
+}
+
+function checkUserGuess(userInput) {
+  if (userInput === gameSelectedLetter) {
+    winCounter++;
+    console.log(`Wins: ${winCounter}`);
+    $("#winCounter").text(winCounter);
+    startGame();
+  } else {
+    wrongGuess(userInput);
+  }
+}
+
+function wrongGuess(userInput) {
+  remainingUserGuesses--;
+  $("#guessesLeftCounter").text(remainingUserGuesses);
+  wrongUserGuessesArray.push(userInput);
+  checkRemainingGuesses();
+}
+
+
+
+function checkRemainingGuesses() {
+  if (remainingUserGuesses === 0) {
+    loseCounter++;
+    $("#lossCounter").text(loseCounter);
+    resetGuessCounter();
+    startGame();
+  }
+}
+function resetGuessCounter() {
+  remainingUserGuesses = 10;
+}
 
 startGame();
 //  Function for capturing key clicks.
 document.onkeyup = function (event) {
-    if (event.keyCode >= 65 && event.keyCode <= 90) {
-      let userInput = event.key.toUpperCase();
-        console.log(`This is User Input: ${userInput}`);
-        checkUserGuess(userInput);
-    }
-  };
+  if (event.keyCode >= 65 && event.keyCode <= 90) {
+    let userInput = event.key.toUpperCase();
+    console.log(`This is User Input: ${userInput}`);
+    checkUserGuess(userInput);
+  }
+};
